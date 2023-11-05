57cb8444fac9f2dd47c2598d0ec2982b6d4b002c
Kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-11-05 20:43:56 +0200

    Add cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,16 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+# config/initializers/cors.rb
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://localhost:3001' # Add the origin of your React frontend
+  
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
+  end
+  
